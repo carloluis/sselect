bf1665d1a289f6698db9436c0f03067596e6712e
carloluis <carloluisr@gmail.com>
2016-10-30 21:09:12 -0300

    + dependencies, webpack init config, indices..

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+let example = (
+    <div className='example'>
+        SearchSelect
+    </div>
+);
+
+ReactDOM.render(
+    example,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = 'development';
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+var EnvPluginConfig = new webpack.DefinePlugin({
+    'process.env':{
+        'NODE_ENV': JSON.stringify(NODE_ENV)
+    }
+});
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+        sourceMapFilename: '[file].map'
+    },
+    module: {
+        loaders: [
+            { 
+            	test: /\.js$/, 
+            	exclude: /node_modules/, 
+            	loader: 'babel-loader', 
+            	query: { 
+            		presets: ['react', 'es2015']
+                },
+        		cacheDirectory: true
+        	}
+        ]
+    },
+    plugins: [HTMLWebpackPluginConfig, EnvPluginConfig],
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devtool: '#source-map',
+    devServer: { 
+        inline: true,
+        port: 8080,
+        colors: true,
+        progress: true,
+        historyApiFallback: true,
+        contentBase: './dist',
+        open: true
+    },
+    watch: true
+}
