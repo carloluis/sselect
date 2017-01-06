63c7e00728e68c56503a523cdb28cea3b7a364ca
carloluis <carloluisr@gmail.com>
2017-01-06 19:46:53 -0300

    + SimpleSelect Component

---
+++ src/components/SimpleSelect.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import ClickOutside from './ClickOutside';
+
+const ITEMS = [
+	{ value: 1, text: 'uno' },
+	{ value: 2, text: 'dos' },
+	{ value: 'html', text: '<b>bold</b>'}
+];
+
+const ITEM = {};
+
+const SPIN_STYLES = {
+	position: 'absolute',
+	cursor: 'pointer',
+	margin: 'auto',
+	color: '#ccc',
+	fontSize:16,
+	right:10,
+	top: 0,
+	bottom: 0,
+	height: 14,
+	zIndex:4
+};
+const X_STYLES = {
+	...SPIN_STYLES,
+	fontSize: 14,
+	right: 30
+};
+const getListStyles = (visible) => ({
+	display: visible? 'block':'none',
+	maxHeight: 190,
+	minWidth: 30,
+	width: 'inherit',
+	overflow: 'auto',
+	cursor: 'pointer'
+});
+
+export class SimpleSelect extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			searchTerm: '',
+			item: props.item,
+			open: false
+		};
+		this.handleClickOut = this.handleClickOut.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+	}
+	handleClickOut(e){
+		this.setState({ open: false });
+	}
+	handleClick(e){
+		this.setState({ open: !this.state.open });
+	}
+	handleChange(e){
+		this.setState({ searchTerm: e.target.value });
+	}
+	removeItem(){
+		this.setState({ item: ITEM });
+	}
+	_render(open, items){
+		return (
+			<ul className="dropdown-menu" style={getListStyles(open)}>
+				{ items.map((item, i) => <li key={i} value={item.value} onClick={()=>this.setState({item})}>{item.text}</li>) }
+			</ul>
+		);
+	}
+	render() {
+		let { open, searchTerm, item, item: { text } } = this.state;
+		let value = open? searchTerm: text;
+		let { style, items } = this.props;
+
+		return (
+			<ClickOutside onClickOutside={this.handleClickOut}>
+				<div className="input-group" style={style} onClick={this.handleClick}>
+					<input type="text" className="form-control" value={value} onChange={this.handleChange} />
+					<span className="clearer glyphicon glyphicon-triangle-bottom" style={SPIN_STYLES} />
+					{ !open && value && <span className="clearer glyphicon glyphicon-remove" style={X_STYLES} onClick={this.removeItem}/> }
+					{ this._render(open, items) }
+				</div>
+			</ClickOutside>
+		);
+	}
+}
+
+SimpleSelect.propTypes = {
+	items: React.PropTypes.arrayOf(React.PropTypes.shape({
+		value: React.PropTypes.any,
+		text: React.PropTypes.any
+	})),
+	item: React.PropTypes.shape({
+		value: React.PropTypes.any,
+		text: React.PropTypes.any
+	})
+};
+SimpleSelect.defaultProps = {
+	items: ITEMS,
+	item: ITEM
+};
+
+
+
+export default SimpleSelect;
