5b9385c7b88340af752764a958549b676429b803
carloluis <carloluisr@gmail.com>
2017-01-07 18:01:25 -0300

    index update.. * example with: ClickOutside, SearchSelect, SimpleSelect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SearchSelect from './components/SearchSelect'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchSelect from './components/SearchSelect';
+import ClickOutside from './components/ClickOutside';
+import SimpleSelect from './components/SimpleSelect';
+
+const root_dom = document.getElementById('app');
+window.root_dom = root_dom;
 
 let items = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
 
+const Sep = ({top=20}) => <div style={{paddingTop:top}} />
+
 let example = (
-    <div className='example'>
-        <SearchSelect items={items} />
-    </div>
+	<div className='example' >
+		<h4>ClickOutside>SearchSelect</h4>
+		<ClickOutside node={root_dom} onClickOutside={()=>console.log('SearchSelect 1')}>
+			<SearchSelect items={items} />	
+		</ClickOutside>
+		<Sep top={200} />
+
+		<h4>SearchSelect</h4>
+		<SearchSelect items={items} />
+		<Sep top={200} />
+		<h4>SimpleSelect</h4>
+		<SimpleSelect style={{width:200}} />
+	</div>
 );
 
 ReactDOM.render(
