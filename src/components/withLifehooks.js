32100077fcc7c3386045295d6ae6869ddf9675df
carloluis <carloluisr@gmail.com>
2017-01-10 09:25:27 -0300

    withLifehooks update..

--- src/components/withLifehooks.js
+++ src/components/withLifehooks.js
@@ -17,38 +17,38 @@ const withLifehooks = ({ componentWillMount, componentDidMount, componentWillRec
 			if (checkFunction(componentDidMount)) {
 				componentDidMount();
 			}
-			super.componentDidMount();
+			super.componentDidMount && super.componentDidMount();
 		}
-		/*componentWillReceiveProps(nextProps) {
+		componentWillReceiveProps(nextProps) {
 			if (checkFunction(componentWillReceiveProps)) {
 				componentWillReceiveProps(nextProps);
 			}
-			super.componentWillReceiveProps(nextProps);
+			super.componentWillReceiveProps && super.componentWillReceiveProps(nextProps);
 		}
 		shouldComponentUpdate(nextProps, nextState) {
 			if (checkFunction(shouldComponentUpdate)) {
 				shouldComponentUpdate(nextProps, nextState);
 			}
-			return super.shouldComponentUpdate(nextProps, nextState);
+			return (super.shouldComponentUpdate && super.shouldComponentUpdate(nextProps, nextState)) || true;
 		}
 		componentWillUpdate(nextProps, nextState) {
 			if (checkFunction(componentWillUpdate)){
 				componentWillUpdate(nextState, nextProps);
 			}
-			super.componentWillUpdate(nextProps, nextState);
+			super.componentWillUpdate && super.componentWillUpdate(nextProps, nextState);
 		}
 		componentDidUpdate(prevProps, prevState) {
 			if (checkFunction(componentDidUpdate)) {
 				componentDidUpdate(prevProps, prevState);
 			}
-			super.componentDidUpdate(prevProps, prevState);
+			super.componentDidUpdate && super.componentDidUpdate(prevProps, prevState);
 		}
 		componentWillUnmount() {
 			if (checkFunction(componentWillUnmount)) {
 				componentWillUnmount();
 			}
-			super.componentWillUnmount();
-		}*/
+			super.componentWillUnmount && super.componentWillUnmount();
+		}
 		render (){
 			return super.render();
 		}
